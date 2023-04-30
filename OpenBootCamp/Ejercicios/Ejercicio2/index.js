0804bd3b2d93c53dd4ec9c5c4be7e19612eeafee
Lucas <115429096+Lucas303030@users.noreply.github.com>
2023-04-30 03:12:37 -0300

    Agrega

---
+++ OpenBootCamp/Ejercicios/Ejercicio2/index.js
@@ -0,0 +1,13 @@
+let array = ["Lucas",
+    33,
+    true,
+    new Date(1990,1,7),
+    {
+        titulo: "Rebelion en la granja",
+        autor: "George Orwell",
+        fecha: new Date(1945, 7, 17),
+        url: "............"
+    }
+    ]
+
+    array
