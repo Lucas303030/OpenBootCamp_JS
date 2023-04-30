1387b1d9686dfe8feaa7aa68e34a317a1a43f4d0
Lucas <115429096+Lucas303030@users.noreply.github.com>
2023-04-30 04:11:18 -0300

    Agrega

---
+++ OpenBootCamp/Ejercicios/Ejercicio3/factorial-break.js
@@ -0,0 +1,12 @@
+let factorial = 10;
+let index = 1;
+
+while (true) {
+    factorial *= index;
+    index ++;
+    if (index > 10) {
+        break
+    }
+    console.log(factorial)
+    
+}
---
+++ OpenBootCamp/Ejercicios/Ejercicio3/factorial-for.js
@@ -0,0 +1,6 @@
+let factorial = 1
+
+for (let index = 1; index <= 10; index++) {
+    factorial *= index;
+    console.log(factorial)
+}
